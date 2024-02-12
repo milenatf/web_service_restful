b2bd68c8515cdd28c7272c3bb8e8d044956eaa76
milena_20 <milena.tavares@maiatecnologia.com.br>
2024-02-12 20:52:38 -0300

    Finalização do módulo 03 - aula 28.

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use App\Http\Requests\StoreUpdateProductFormRequest;
-
 use App\Models\Product;
+
 
 class ProductController extends Controller
 {
     private $product;
     private $totalPage = 50;
+    private $path = 'products';
 
     public function __construct(Product $product)
     {
@@ -35,7 +38,26 @@ class ProductController extends Controller
      */
     public function store(StoreUpdateProductFormRequest $request)
     {
-        $product = $this->product->create($request->all());
+        $data = $request->all();
+
+        // Verifica se existe aquivos para fazer upload
+        if($request->hasFile('image') && $request->file('image')->isValid())
+        {
+            /** O trecho de código $request->name::of('fooBar')->kebab()
+             * convert a string $request->name para kebab_case, ou seja, retira caracteres especiais e espaços do nome do arquivo
+             *
+             * O trecho de código $request->image->extension()
+             * Pega a extensão do arquivo quem vem na request
+             *
+             */
+            $fileName = Str::of($request->name)->kebab().'.'.$request->image->extension();
+            $data['image'] = $fileName;
+
+            if(!$request->file('image')->storeAs("public/{$this->path}", $fileName))
+                return response()->json(['error' => 'Não foi possível fazer o upload da imagem'], 500);
+        }
+
+        $product = $this->product->create($data);
 
         if(!$product)
             return response()->json(['erro' => 'Não foi possível cadastrar o produto!'], 500);
@@ -61,10 +83,41 @@ class ProductController extends Controller
      */
     public function update(StoreUpdateProductFormRequest $request, string $id)
     {
+        $data = $request->all();
+
         $product = $this->product->find($id);
 
         if(!$product)
             return response()->json(['erro' => 'Produto não encontrado'], 404);
+
+        // Verifica se existe aquivos para fazer upload
+        if($request->hasFile('image') && $request->file('image')->isValid())
+        {
+            /** Verifica que existe uma imagem no produto. Se existir, e for realizada uma atualização na imagem, a imagem que já estava armazenada é excluída do diretório e a
+             * nova imagem é inserida.
+             */
+            if( $product->image ) { // Se existir a imagem
+                /**
+                 * A facade Storage do láravel verifica se o arquivo existe no diretório
+                 */
+                if(Storage::exists("{$this->path}/{$product->image}"))
+                    Storage::delete("{$this->path}/{$product->image}"); // Storage::delete é o método que vai deletar o arquivo do diretório
+            }
+
+            /** O trecho de código $request->name::of('fooBar')->kebab()
+             * convert a string $request->name para kebab_case, ou seja, retira caracteres especiais e espaços do nome do arquivo
+             *
+             * O trecho de código $request->image->extension()
+             * Pega a extensão do arquivo quem vem na request
+             *
+             */
+            $fileName = Str::of($request->name)->kebab().'.'.$request->image->getClientOriginalExtension();
+            $data['image'] = $fileName;
+
+            if(!$request->file('image')->storeAs("public/{$this->path}", $fileName))
+                return response()->json(['error' => 'Não foi possível fazer o upload da imagem'], 500);
+        }
+
 
         if(!$product->update($request->all()))
             return response()->json(['erro' => 'Não foi possível atualizar o produto'], 500);
